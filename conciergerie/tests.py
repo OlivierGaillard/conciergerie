61a28e2be26b11a9d0b08bb6eb0e6bbf4b79344a
info@metacoherence.ch <info@metacoherence.ch>
2017-07-16 19:12:14 +0200

    Essaie

--- conciergerie/tests.py
+++ conciergerie/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from conciergerie.forms import TravailFilterForm
 # Create your tests here.
+class TestValidation(TestCase):
+
+    def test_month_zero(self):
+        form = TravailFilterForm({'month' : 1})
+        self.assertFalse(form.is_valid())
+
+
+
+
